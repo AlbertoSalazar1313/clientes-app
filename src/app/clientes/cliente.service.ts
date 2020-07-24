2db7a81cfee45943d76c97e7ecab320cbd428ec7
Alberto Salazar <engelraiden4@gmail.com>
2020-07-24 17:21:38 -0500

    Se puede actualizar y registrar clientes. Ademas ya se hace conexion con base de datos, por lo cual se necesita el springboot para que funcione

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,15 +2,30 @@ import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
 import { CLIENTES } from './clientes.json';
 import { of, Observable } from 'rxjs';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
+  private urlEndPoint: string = 'http://localhost:8084/api/clientes';
 
-  constructor() { }
+  private httpHeaders = new HttpHeaders({'Content-Type':'application/json'});
+
+  constructor(private http: HttpClient) { }
 
   getClientes():Observable<Cliente[]>{
-    return of(CLIENTES);
+    return this.http.get<Cliente[]>(this.urlEndPoint);
   }
+  create(cliente: Cliente): Observable<Cliente>{
+    return this.http.post<Cliente>(this.urlEndPoint, cliente,{headers: this.httpHeaders});
+  }
+  getCliente(id): Observable<Cliente>{
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`);
+  }
+  update(cliente: Cliente): Observable<Cliente>{
+    return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`,cliente,{headers: this.httpHeaders});
+  }
+
+
 }
---
+++ src/app/clientes/form.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
+import { Router,ActivatedRoute } from '@angular/router';
+import swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html'
+})
+export class FormComponent implements OnInit {
+
+  private cliente: Cliente = new Cliente();
+  private titulo: string = "Registrar Cliente";
+
+  constructor(private clienteService: ClienteService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.cargarcliente();
+  }
+
+  cargarcliente(): void{
+    this.activatedRoute.params.subscribe(
+      params => {
+        let id = params['id']
+        if(id){
+          this.clienteService.getCliente(id).subscribe((cliente) => this.cliente = cliente)
+        }
+      }
+    )
+  }
+
+  public create(): void{
+    this.clienteService.create(this.cliente).subscribe(
+      response => {
+        this.router.navigate(['/clientes'])
+        swal.fire('Nuevo Cliente',`Cliente ${this.cliente.nombre} registrad@ con exito!`,'success')
+      }
+    )
+    console.log("clicked");
+    console.log(this.cliente);
+  }
+  update():void{
+    this.clienteService.update(this.cliente).subscribe(
+      cliente =>{
+        this.router.navigate(['/clientes'])
+        swal.fire('Cliente Actualizado',`Cliente ${this.cliente.nombre} actualizad@ con exito!`,'success')
+      }
+    )
+  }
+
+}
