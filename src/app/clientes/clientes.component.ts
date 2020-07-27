8a70dd85f32faf2cc7d8ff193c864d936580b357
Alberto Salazar <engelraiden4@gmail.com>
2020-07-27 10:00:27 -0500

    .

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
- import { Component, OnInit } from '@angular/core';
- import { Cliente } from './cliente';
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -18,5 +19,30 @@ export class ClientesComponent implements OnInit {
         clientes => this.clientes=clientes
       );
   }
+  delete(cliente: Cliente): void{
+      Swal.fire({
+    title: 'Estas seguro?',
+    text: `Seguro qué desea eliminar al cliente ${cliente.nombre} ${cliente.apellido}?`,
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Si',
+    cancelButtonText: 'No'
+    }).then((result) => {
+    if (result.value) {
+      this.clienteService.delete(cliente.id).subscribe(
+        response => {
+          this.clientes = this.clientes.filter(cli => cli !== cliente)
+          Swal.fire(
+            'Cliente eliminado',
+            `Se ha eliminado exitosamente al cliente ${cliente.nombre} ${cliente.apellido}`,
+            'success'
+        )
+        }
 
+      )
+  }
+})
+  }
 }
